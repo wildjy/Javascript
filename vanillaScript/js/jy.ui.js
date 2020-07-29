a77d685304dc95eed40b5b8e9d757d8d7302b0c6
jaeyoungYu <lucky0914@jinhak.com>
2020-07-29 18:07:28 +0900

    tab Module

--- vanillaScript/js/jy.ui.js
+++ vanillaScript/js/jy.ui.js
@@ -86,6 +86,59 @@ var ui_Project = {
   },
 
   ui : {
+    tabModule : function($tab, $panel, $cls, $callback){
+      var _target = document.querySelector($tab);
+      var _tab = _target.getElementsByTagName("li");
+      var _panel = document.querySelectorAll($panel);
+      //console.log(_target)
+      console.log(_tab)
+      console.log(_panel)
+
+
+      Array.prototype.forEach.call(_tab, function(tab, index) {
+        var a = tab.getAttribute('class').indexOf('selected');
+        var b = [a];
+        console.log(b.indexOf(true))
+        if(a === true){
+
+        }
+        //_panel[_isTab].style.display = 'block';
+      });
+
+      function hidePanel(){
+        Array.prototype.forEach.call(_panel, function(panelList, index) {
+          panelList.style.display = 'none';
+        });
+      };
+
+      hidePanel();
+
+      // call
+      Array.prototype.forEach.call(_tab, function(tabList, index) {
+        //console.log(tabList);
+        console.log(index);
+        tabList.children[0].setAttribute("data-tabNum", index); //data-tabNum setting
+
+        _panel[0].style.display = 'block'; // first tab panel show
+        tabList.children[0].addEventListener('click', function(e) {
+          var _isTab = this.getAttribute('data-tabNum'); //data-tabNum search
+          //console.log(this);
+          //console.log(_isTab);
+          // tab
+          Array.prototype.forEach.call(_tab, function(tabList, index) {
+            removeClass(tabList, $cls);
+          });
+          // tab panel
+          hidePanel();
+          // active
+          addClass(this.parentElement, $cls);
+          _panel[_isTab].style.display = 'block';
+
+          e.preventDefault()
+        })
+      })
+    },
+
     scroll : function($standard, $target, $fixed, $class, $type){
       //$standard(script 유무), $target(fixed relative), $fixed(fixed target), $class(fix addClass), $type[default, menu]
       var _standard = document.querySelector($standard);
