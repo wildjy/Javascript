c0702ee7f9f1f729c695b9fbf9a4ff236cd42fe4
jaeyoungYu <lucky0914@jinhak.com>
2022-05-20 11:14:10 +0900

    input error check script

--- vanillaScript/js/jy.ui.js
+++ vanillaScript/js/jy.ui.js
@@ -24,54 +24,132 @@ function addActive(obj, cls, callback){
 function removeActive(obj, cls, callback){
 	removeClass(document.getElementById(obj), cls);
 };
-// this addClass
-function thisAddActive(_this, cls, callback){
-	addClass(_this, cls);
-}
-// this removeClass
-function thisRemoveActive(_this, cls, callback){
-	removeClass(_this, cls);
-};
-// toggle active
-function toggleActive(_this, obj, cls, callback){
-	if(hasClass(document.getElementById(obj), cls)){
-    removeClass(_this, cls);
-		removeClass(document.getElementById(obj), cls);
-	} else {
-    addClass(_this, cls);
-		addClass(document.getElementById(obj), cls);
-	}
-};
-// element show
-function overShow(obj, cls, type, callback){
-	addClass(document.getElementById(obj), cls);
-	document.getElementById(obj).style.display = type;
-};
-// element hide
-function overHide(obj, cls, callback){
-	removeClass(document.getElementById(obj), cls);
-	document.getElementById(obj).style.display = "none";
-};
-// toggle element show/hide
 function toggleShow(_this, obj, cls, type, callback){
 	if(hasClass(document.getElementById(obj), cls)){
-    removeClass(_this, cls);
+		_this.classList.remove(cls);
 		removeClass(document.getElementById(obj), cls);
 		document.getElementById(obj).style.display = "none";
 	} else {
-    addClass(_this, cls);
+		_this.classList.add(cls);
 		addClass(document.getElementById(obj), cls);
 		document.getElementById(obj).style.display = type;
 	}
-};
-
-// iframe out element show
+}
+function overShow(obj, cls, type, callback){
+	addClass(document.getElementById(obj), cls);
+	document.getElementById(obj).style.display = type;
+}
 function iframeShow(obj, cls, type, callback){
 	//console.log(obj);
+	//console.log('inner.html �� �⑥닔');
 	//console.log($('#info', parent.document));
 	var test = $("#" + obj, parent.document);
 	test.addClass(cls);
-	test.css({"display" : type});
+	test.css({"display":type});
+}
+function iframeHide(obj, cls, type, callback){
+	//console.log(obj);
+	//console.log('inner.html �� �⑥닔');
+	//console.log($('#info', parent.document));
+	var test = $("#" + obj, parent.document);
+	test.removeClass(cls);
+	test.css({"display":type});
+}
+function overHide(obj, cls, callback){
+	removeClass(document.getElementById(obj), cls);
+	document.getElementById(obj).style.display = "none";
+}
+//addClass, removeClass
+function toggleActive(_this, obj, cls, callback){
+	if(hasClass(document.getElementById(obj), cls)){
+		_this.classList.remove(cls);
+		removeClass(document.getElementById(obj), cls);
+	} else {
+		_this.classList.add(cls);
+		addClass(document.getElementById(obj), cls);
+	}
+}
+function addActive(obj, cls, callback){
+	addClass(document.getElementById(obj), cls);
+}
+function removeActive(obj, cls, callback){
+	removeClass(document.getElementById(obj), cls);
+}
+function thisAddActive(_this, cls, callback){
+	_this.classList.add(cls);
+	//addClass(document.getElementById(obj), cls);
+}
+function thisRemoveActive(_this, cls, callback){
+	_this.classList.remove(cls);
+	//removeClass(document.getElementById(obj), cls);
+}
+
+//fadeIn
+function fadeIn(target){
+	var level = 0;
+	var inTimer = null;
+	inTimer = setInterval(function(){
+		level = fadeInAction(target, level, inTimer);
+	}, 50)
+};
+function fadeInAction(target, level, inTimer){
+	level = level + 0.1;
+	changeOpacity(target, level);
+	target.style.display = "block"
+	if(level > 1) {
+		clearInterval(inTimer);
+	} else {
+		return level;
+	}
+}
+//fadeOut
+function fadeOut(target){
+	var level = 1;
+	var outTimer = null;
+	inTimer = setInterval(function(){
+		level = fadeInAction(target, level, outTimer);
+	}, 50)
+};
+function fadeOutAction(target, level, outTimer){
+	level = level - 0.1;
+	changeOpacity(target, level);
+	target.style.display = "none"
+	if(level<0) {
+		clearInterval(outTimer);
+	} else {
+	return level;
+	}
+}
+function changeOpacity(target, level){
+	var obj = target;
+	obj.style.opacity = level;
+	obj.style.MozOpacity = level;
+	obj.style.KhtmlOpacity = level;
+	obj.style.MsFilter = "'progid: DXImageTransform.Microsoft.Alpha(Opacity=" + (level * 100) + ")'";
+	obj.style.filter = "alpha(opacity=" + (level * 100) + ");";
+}
+
+function toolTip(target, cls, type, callback){
+	//target : this, cls : class, type : close
+	switch (type){
+		case "close" : //close
+			//console.log("close");
+			removeClass(target.parentNode.parentNode.previousElementSibling , cls);
+			removeClass(target.parentNode.parentNode, cls);
+		break;
+		default : //default
+			//console.log("default");
+			if(hasClass(target, cls)){
+				removeClass(target, cls);
+				removeClass(target.nextElementSibling, cls);
+				//target.nextElementSibling.classList.remove(cls);
+			} else {
+				addClass(target, cls);
+				addClass(target.nextElementSibling, cls);
+				//target.nextElementSibling.classList.add(cls);
+			}
+		break;
+	};
 }
 
 // Get a index number from siblings
@@ -96,66 +174,95 @@ var ui_Project = {
   },
 
   ui : {
-    tabModule : function($tab, $panel, $cls, $callback){
+    tabModule : function($tab, $panel, $cls, $type, $callback){
       var _target = document.querySelector($tab);
-      var _tab = _target.getElementsByTagName("li");
+      var _tab = _target.querySelectorAll("li");
       var _panel = document.querySelectorAll($panel);
-      //console.log(_target)
-      //console.log(_tab.getElementsByClassName("selected"))
-      console.log(_panel)
 
-      function getElementIndex(element, range) {
-        // 추가
-        if (!!range) return [].indexOf.call(element, range);
-        return [].indexOf.call(element.parentNode.children, element);
-      }
+      console.log(_tab);
+			switch ($type){
+      case "reset" : //reset
+        //console.log("reset");
+        var _allTab = document.querySelectorAll($tab);
 
-      var ele = document.getElementById('common_Tab').getElementsByClassName('selected');
-      var testElements = document.getElementsByClassName('selected');
-      var testDivs = Array.prototype.filter.call(testElements, function(testElement) {
-        //console.log(testElement.index);
-        return testElement.getIndex;
-      });
-      Array.prototype.forEach.call(_tab, function(tab, index) {
-        //console.log(tab.setAttribute("data-tabNum", index));
-        //console.log(tab.setAttribute("data-tabNum", index));
-        console.log(index);
+        console.log(_allTab)
+        console.log(_allTab.children)
 
-        // _panel[_isTab].style.display = 'block';
-      });
+				Array.prototype.forEach.call(_allTab, function(tab, index) {
+          console.log(tab.children)
+          console.log(tab.children[0])
 
-      function hidePanel(){
-        Array.prototype.forEach.call(_panel, function(panelList, index) {
-          panelList.style.display = 'none';
-        });
-      };
+          for(var i=0; i < tab.children.length; i++){
+            if(tab.children[i].nodeType != 1) continue
+            var items = tab.children.item(i);
+            items.classList.remove($cls);
+          }
+          addClass(tab.children[0], $cls);
+				});
 
-      hidePanel();
+				Array.prototype.forEach.call(_tab, function(tab, index) {
+          removeClass(tab, $cls);
+				});
+				Array.prototype.forEach.call(_panel, function(panelList, index) {
+					panelList.style.display = 'none';
+				});
 
-      // call
-      Array.prototype.forEach.call(_tab, function(tabList, index) {
-        //console.log(tabList);
-        //console.log(index);
-        tabList.children[0].setAttribute("data-tabNum", index); //data-tabNum setting
+        addClass(_tab[0], $cls);
+				_panel[0].style.display = 'block';
+        break;
+      case "hide" : // hide
+        //console.log("hide");
 
-        _panel[0].style.display = 'block'; // first tab panel show
-        tabList.children[0].addEventListener('click', function(e) {
-          var _isTab = this.getAttribute('data-tabNum'); //data-tabNum search
-          console.log(this);
-          //console.log(_isTab);
-          // tab
-          Array.prototype.forEach.call(_tab, function(tabList, index) {
-            removeClass(tabList, $cls);
+				Array.prototype.forEach.call(_tab, function(tab, index) {
+          removeClass(tab, $cls);
+				});
+				Array.prototype.forEach.call(_panel, function(panelList, index) {
+					panelList.style.display = 'none';
+				});
+
+        break;
+      default : //init Event
+
+        function hidePanel(){
+          Array.prototype.forEach.call(_panel, function(panelList, index) {
+            panelList.style.display = 'none';
           });
-          // tab panel
-          hidePanel();
-          // active
-          addClass(this.parentElement, $cls);
-          _panel[_isTab].style.display = 'block';
+        };
 
-          e.preventDefault()
-        })
-      })
+        hidePanel();
+
+        Array.prototype.forEach.call(_tab, function(tabList, index) {
+          //console.log(tabList);
+          //console.log(index);
+          tabList.children[0].setAttribute("data-tabNum", index); //data-tabNum setting
+
+          //_panel[0].style.display = 'block'; // first tab panel show
+          tabList.children[0].addEventListener('click', function(e) {
+            var _isTab = this.getAttribute('data-tabNum'); //data-tabNum search
+            //console.log(this);
+            //console.log(_isTab);
+
+            // tab
+            Array.prototype.forEach.call(_tab, function(tabList, index) {
+              removeClass(tabList, $cls);
+            });
+            // tab panel
+            hidePanel();
+            // active
+            addClass(this.parentElement, $cls);
+            _panel[_isTab].style.display = 'block';
+
+            e.preventDefault()
+          })
+        });
+
+        //active selected
+        var _active = _target.querySelectorAll("li.selected")[0];
+        var _activeNum = _active.firstChild.getAttribute('data-tabNum');
+        _panel[_activeNum].style.display = 'block';
+
+        break;
+      };
     },
 
     scroll : function($standard, $target, $fixed, $class, $type){
